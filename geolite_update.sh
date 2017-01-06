909805e7029b87b2135696ab75fb262b5aeb86e7
Gianluca Costa <g.costa@xplico.org>
2017-01-06 11:06:12 +0100

    Script to download GeoLiteCity DBs

---
+++ geolite_update.sh
@@ -0,0 +1,4 @@
+wget http://geolite.maxmind.com/download/geoip/database/GeoLiteCity.dat.gz
+wget http://geolite.maxmind.com/download/geoip/database/GeoLiteCityv6-beta/GeoLiteCityv6.dat.gz
+gunzip GeoLiteCity.dat.gz
+gunzip GeoLiteCityv6.dat.gz
