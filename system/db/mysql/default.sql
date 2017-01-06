8a61cac66bb533863c7f82082ec390a7426987e8
Gianluca Costa <g.costa@xplico.org>
2017-01-06 11:05:31 +0100

    Improved the default SQLite DB configuration

--- system/db/mysql/default.sql
+++ system/db/mysql/default.sql
@@ -5,4 +5,6 @@ INSERT INTO groups VALUES(2, "users");
 
 INSERT INTO users VALUES(1, "admin", "4295173f5047ed4edf9eee2a047abc33", "demo1@xplico.org", 1, "none", "Xplico", "Team", "0", 0, 'FULL', 1, 1, 1, 0);
 INSERT INTO users VALUES(2, "xplico", "4295173f5047ed4edf9eee2a047abc33", "demo2@xplico.org", 1, "none", "Xplico", "Team", "0", 0, 'NORMAL', 1, 1, 2, 0);
+UPDATE users SET quota_used=0;
+-- UPDATE params SET nvalue=1 WHERE name="register";
 
