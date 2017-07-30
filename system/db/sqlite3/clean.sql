22e9dcd3205a25255d20e6f5aa3c9277068d5529
Gianluca Costa <g.costa@xplico.org>
2017-07-30 11:12:50 +0200

    Bugfix: Clear SQLite DB in right sequence

--- system/db/sqlite3/clean.sql
+++ system/db/sqlite3/clean.sql
@@ -1,8 +1,8 @@
 --- clean all tables except users and groups
 
 PRAGMA foreign_keys = ON;
-DELETE FROM pols;
 DELETE FROM sols;
+DELETE FROM pols;
 UPDATE users SET quota_used=0;
 UPDATE params SET nvalue=1 WHERE name="register";
 
